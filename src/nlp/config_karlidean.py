63e469e1ce2dd9749f7c274957385f8f8ddec038
Karli Dean <karlidean11@gmail.com>
2026-04-23 20:58:02 -0500

    WF 5.

--- src/nlp/config_karlidean.py
+++ src/nlp/config_karlidean.py
@@ -33,8 +33,7 @@ from pathlib import Path
 # API CONFIGURATION
 # ============================================================
 
-# TODO: In your custom app, change the URL to work with a different page.
-PAGE_URL: str = "https://arxiv.org/abs/2602.20021"
+PAGE_URL: str = "https://arxiv.org/abs/2604.21794"
 
 # Let them know who we are (and that we're doing educational web mining).
 HTTP_REQUEST_HEADERS: dict = {
